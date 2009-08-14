a585a7d98c1260c5e30bd2b1e6f2685c4deb6ddb
Dan Armstrong <dan@aoindustries.com>
2009-08-14 20:59:46 +0000

    LGPLv3

--- src/com/aoindustries/taglib/TextTag.java
+++ src/com/aoindustries/taglib/TextTag.java
@@ -1,10 +1,27 @@
+/*
+ * aocode-public-taglib - Reusable Java taglib of general tools with minimal external dependencies.
+ * Copyright (C) 2009  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of aocode-public-taglib.
+ *
+ * aocode-public-taglib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aocode-public-taglib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with aocode-public-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.aoindustries.taglib;
 
-/*
- * Copyright 2009 by AO Industries, Inc.,
- * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
- * All rights reserved.
- */
 import com.aoindustries.encoding.MediaEncoder;
 import com.aoindustries.encoding.MediaType;
 import com.aoindustries.encoding.TextInXhtmlEncoder;
