911922bbdc49f4d097ab80da65f5871d08bb67ac
Dan Armstrong <dan@aoindustries.com>
2010-04-08 19:21:34 +0000

    Added select tag with onchange attribute.

---
+++ src/com/aoindustries/taglib/SelectTag.java
@@ -0,0 +1,81 @@
+/*
+ * aocode-public-taglib - Reusable Java taglib of general tools with minimal external dependencies.
+ * Copyright (C) 2010  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of aocode-public-taglib.
+ *
+ * aocode-public-taglib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aocode-public-taglib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with aocode-public-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.taglib;
+
+import com.aoindustries.encoding.JavaScriptInXhtmlAttributeEncoder;
+import com.aoindustries.encoding.MediaType;
+import com.aoindustries.io.StringBuilderWriter;
+import com.aoindustries.util.EncodingUtils;
+import java.io.IOException;
+import java.io.Writer;
+import javax.servlet.jsp.JspException;
+
+/**
+ * @author  AO Industries, Inc.
+ */
+public class SelectTag extends AutoEncodingBufferedTag implements NameAttribute, OnchangeAttribute {
+
+    private String name;
+    private String onchange;
+
+    public MediaType getContentType() {
+        return MediaType.XHTML;
+    }
+
+    public MediaType getOutputType() {
+        return MediaType.XHTML;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOnchange() {
+        return onchange;
+    }
+
+    public void setOnchange(String onchange) {
+        this.onchange = onchange;
+    }
+
+    protected void doTag(StringBuilderWriter capturedBody, Writer out) throws JspException, IOException {
+        out.write("<select");
+        if(name!=null) {
+            out.write(" name=\"");
+            EncodingUtils.encodeXmlAttribute(name, out);
+            out.write('"');
+        }
+        if(onchange!=null) {
+            out.write(" onchange=\"");
+            JavaScriptInXhtmlAttributeEncoder.encodeJavaScriptInXhtmlAttribute(onchange, out);
+            out.write('"');
+        }
+        out.write('>');
+        out.write(capturedBody.toString());
+        out.write("</select>");
+    }
+}
