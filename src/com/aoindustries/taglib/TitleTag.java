0b80fd17ad52404043d854fc65354356628650f8
Dan Armstrong <dan@aoindustries.com>
2010-07-04 07:00:25 +0000

    Converting DAO layer

---
+++ src/com/aoindustries/taglib/TitleTag.java
@@ -0,0 +1,54 @@
+/*
+ * aocode-public-taglib - Reusable Java taglib of general tools with minimal external dependencies.
+ * Copyright (C) 2010  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of aocode-public-taglib.
+ *
+ * aocode-public-taglib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aocode-public-taglib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with aocode-public-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.taglib;
+
+import com.aoindustries.encoding.MediaType;
+import com.aoindustries.io.StringBuilderWriter;
+import java.io.IOException;
+import java.io.Writer;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.JspTag;
+
+/**
+ * @author  AO Industries, Inc.
+ */
+public class TitleTag extends AutoEncodingBufferedTag {
+
+    @Override
+    public MediaType getContentType() {
+        return MediaType.TEXT;
+    }
+
+    @Override
+    public MediaType getOutputType() {
+        return null;
+    }
+
+    @Override
+    protected void doTag(StringBuilderWriter capturedBody, Writer out) throws JspException, IOException {
+        JspTag parent = findAncestorWithClass(this, TitleAttribute.class);
+        if(parent==null) throw new JspException(ApplicationResources.accessor.getMessage("TitleTag.needTitleAttributeParent"));
+        TitleAttribute titleAttribute = (TitleAttribute)parent;
+        titleAttribute.setTitle(capturedBody.toString().trim());
+    }
+}
