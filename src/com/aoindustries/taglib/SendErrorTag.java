8c033b2ca20d80160fdbead6a45dec837f2224c6
Dan Armstrong <dan@aoindustries.com>
2012-03-04 09:07:54 +0000

    New sendError tag.

---
+++ src/com/aoindustries/taglib/SendErrorTag.java
@@ -0,0 +1,80 @@
+/*
+ * aocode-public-taglib - Reusable Java taglib of general tools with minimal external dependencies.
+ * Copyright (C) 2012  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of aocode-public-taglib.
+ *
+ * aocode-public-taglib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aocode-public-taglib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with aocode-public-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.taglib;
+
+import com.aoindustries.encoding.MediaType;
+import com.aoindustries.io.AutoTempFileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.SkipPageException;
+
+/**
+ * @author  AO Industries, Inc.
+ */
+public class SendErrorTag extends AutoEncodingBufferedTag {
+
+    private int status;
+    private String message;
+
+    @Override
+    public MediaType getContentType() {
+        return MediaType.TEXT;
+    }
+
+    @Override
+    public MediaType getOutputType() {
+        return null;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    protected void doTag(AutoTempFileWriter capturedBody, Writer out) throws JspException, IOException {
+        if(message==null) message = capturedBody.toString().trim();
+
+        PageContext pageContext = (PageContext)getJspContext();
+        HttpServletResponse response = (HttpServletResponse)pageContext.getResponse();
+
+        if(message==null || message.isEmpty()) response.sendError(status);
+        else response.sendError(status, message);
+
+        throw new SkipPageException();
+    }
+}
