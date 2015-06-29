1a119742919425fa1f1134ca13b78985acd96547
Dan Armstrong <dan@aoindustries.com>
2015-06-29 23:53:29 +0000

    Reusing some parts of aocode-public-taglib in docs-taglib implementation.

--- src/com/aoindustries/taglib/ApplicationResources.java
+++ src/com/aoindustries/taglib/ApplicationResources.java
@@ -1,6 +1,6 @@
 /*
  * aocode-public-taglib - Reusable Java taglib of general tools with minimal external dependencies.
- * Copyright (C) 2009, 2010, 2011, 2013  AO Industries, Inc.
+ * Copyright (C) 2009, 2010, 2011, 2013, 2015  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -55,5 +55,5 @@ public final class ApplicationResources extends EditableResourceBundle {
         );
     }
 
-    static final ApplicationResourcesAccessor accessor = ApplicationResourcesAccessor.getInstance(bundleSet.getBaseName());
+    public static final ApplicationResourcesAccessor accessor = ApplicationResourcesAccessor.getInstance(bundleSet.getBaseName());
 }
