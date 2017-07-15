0ef9a6cfb476af9df80bb90769c0187fd2582a2f
AO Industries, Inc. <support@aoindustries.com>
2017-07-15 15:50:36 -0500

    Renamed properties file to "Maven.properties", created package-private accessor "Maven".

--- book/src/main/java/com/aoindustries/taglib/book/Maven.java
+++ book/src/main/java/com/aoindustries/taglib/book/Maven.java
@@ -1,6 +1,6 @@
 /*
  * ao-taglib - Making JSP what it should have been all along.
- * Copyright (C) 2016, 2017  AO Industries, Inc.
+ * Copyright (C) 2017  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,32 +22,23 @@
  */
 package com.aoindustries.taglib.book;
 
-import com.semanticcms.tagreference.TagReferenceInitializer;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import com.aoindustries.util.PropertiesUtils;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * @author  AO Industries, Inc.
  */
-public class AoTldInitializer extends TagReferenceInitializer {
+class Maven {
 
-	private static final Map<String,String> additionalApiLinks = new LinkedHashMap<String,String>();
+	static final Properties properties;
 	static {
-		// Self
-		additionalApiLinks.put("com.aoindustries.taglib.", Maven.properties.getProperty("documented.url") + "apidocs/");
-		// Dependencies
-		additionalApiLinks.put("com.aoindustries.util.", "https://aoindustries.com/aocode-public/apidocs/");
+		try {
+			properties = PropertiesUtils.loadFromResource(Maven.class, "Maven.properties");
+		} catch(IOException e) {
+			throw new ExceptionInInitializerError(e);
+		}
 	}
 
-	public AoTldInitializer() {
-		super(
-			"AO Taglib Reference",
-			"Taglib Reference",
-			"/ao-taglib",
-			"/ao.tld",
-			Maven.properties.getProperty("javac.link.javaApi.jdk16"),
-			Maven.properties.getProperty("javac.link.javaeeApi.6"),
-			additionalApiLinks
-		);
-	}
+	private Maven() {}
 }
