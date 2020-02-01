f9d3f31fc321b06fdc9e09b5b55bb02d6bf06434
AO Industries, Inc. <support@aoindustries.com>
2020-02-01 08:14:59 -0600

    Action required before HTML 5

--- src/main/java/com/aoindustries/taglib/FormTag.java
+++ src/main/java/com/aoindustries/taglib/FormTag.java
@@ -1,6 +1,6 @@
 /*
  * ao-taglib - Making JSP be what it should have been all along.
- * Copyright (C) 2010, 2011, 2013, 2015, 2016, 2017, 2019  AO Industries, Inc.
+ * Copyright (C) 2010, 2011, 2013, 2015, 2016, 2017, 2019, 2020  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
