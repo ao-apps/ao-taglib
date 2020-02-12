84453002d7e53e23964680677be5f2e74cf85ed9
AO Industries, Inc. <support@aoindustries.com>
2020-02-11 20:59:37 -0600

    Using ao-fluent-html for <col> tags.

---
+++ src/main/java/com/aoindustries/taglib/ColTag.java
@@ -0,0 +1,93 @@
+/*
+ * ao-taglib - Making JSP be what it should have been all along.
+ * Copyright (C) 2020  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-taglib.
+ *
+ * ao-taglib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-taglib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.taglib;
+
+import com.aoindustries.encoding.MediaType;
+import com.aoindustries.html.Col;
+import com.aoindustries.html.servlet.HtmlEE;
+import java.io.IOException;
+import java.io.Writer;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author  AO Industries, Inc.
+ */
+public class ColTag
+	extends AutoEncodingNullTag
+	implements
+		IdAttribute,
+		ClassAttribute,
+		StyleAttribute
+{
+
+	@Override
+	public MediaType getOutputType() {
+		return MediaType.XHTML;
+	}
+
+	private Object id;
+	@Override
+	public void setId(Object id) throws JspTagException {
+		this.id = AttributeUtils.trimNullIfEmpty(id);
+	}
+
+	private int span;
+	public void setSpan(int span) throws JspTagException {
+		this.span = span;
+	}
+
+	private Object clazz;
+	@Override
+	public Object getClazz() {
+		return clazz;
+	}
+
+	@Override
+	public void setClazz(Object clazz) throws JspTagException {
+		this.clazz = AttributeUtils.trimNullIfEmpty(clazz);
+	}
+
+	private Object style;
+	@Override
+	public void setStyle(Object style) throws JspTagException {
+		this.style = AttributeUtils.trimNullIfEmpty(style);
+	}
+
+	@Override
+	protected void doTag(Writer out) throws JspTagException, IOException {
+		PageContext pageContext = (PageContext)getJspContext();
+		Col col = HtmlEE.get(
+			pageContext.getServletContext(),
+			(HttpServletRequest)pageContext.getRequest(),
+			out
+		).col()
+			.id(id);
+		if(span != 0) col.span(span);
+		col
+			.clazz(clazz)
+			.style(style)
+			.__();
+	}
+}
