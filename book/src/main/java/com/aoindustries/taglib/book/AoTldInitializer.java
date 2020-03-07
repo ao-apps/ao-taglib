def3aa7f54b827a11912a81ea55c77087b646b73
AO Industries, Inc <support@aoindustries.com>
2020-03-07 13:25:33 -0600

    Changed default Java version from 1.7 to 1.8 and refactored build properties

--- book/src/main/java/com/aoindustries/taglib/book/AoTldInitializer.java
+++ book/src/main/java/com/aoindustries/taglib/book/AoTldInitializer.java
@@ -42,8 +42,8 @@ public class AoTldInitializer extends TagReferenceInitializer {
 			"Taglib Reference",
 			"/ao-taglib",
 			"/ao.tld",
-			Maven.properties.getProperty("documented.javac.link.javaApi"),
-			Maven.properties.getProperty("javac.link.javaeeApi.6"),
+			Maven.properties.getProperty("documented.javadoc.link.javase"),
+			Maven.properties.getProperty("javadoc.link.javaee.6"),
 			additionalApiLinks
 		);
 	}
