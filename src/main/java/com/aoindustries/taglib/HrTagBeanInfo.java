c7af8d55f34018f574ad4121b646c0150c95a53e
AO Industries, Inc <support@aoindustries.com>
2020-07-27 19:54:13 -0500

    Added BeanInfo for <hr>

---
+++ src/main/java/com/aoindustries/taglib/HrTagBeanInfo.java
@@ -0,0 +1,70 @@
+/*
+ * ao-taglib - Making JSP be what it should have been all along.
+ * Copyright (C) 2020  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-taglib.
+ *
+ * ao-taglib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-taglib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.taglib;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.beans.SimpleBeanInfo;
+
+/**
+ * @author  AO Industries, Inc.
+ */
+public class HrTagBeanInfo extends SimpleBeanInfo {
+
+	private static volatile PropertyDescriptor[] properties = null;
+
+	@Override
+	public PropertyDescriptor[] getPropertyDescriptors () {
+		try {
+			PropertyDescriptor[] props = properties;
+			if(props == null) {
+				props = new PropertyDescriptor[] {
+					// From base class: new PropertyDescriptor("outputType", HrTag.class, "getOutputType", null),
+					new PropertyDescriptor("id",    HrTag.class, null,       "setId"),
+					new PropertyDescriptor("class", HrTag.class, "getClazz", "setClazz"),
+					new PropertyDescriptor("style", HrTag.class, null,       "setStyle"),
+				};
+				properties = props;
+			}
+			return props; // Not copying array for performance
+		} catch(IntrospectionException err) {
+			throw new AssertionError(err);
+		}
+	}
+
+	/**
+	 * Include base class.
+	 */
+	@Override
+	public BeanInfo[] getAdditionalBeanInfo() {
+		try {
+			return new BeanInfo[] {
+				Introspector.getBeanInfo(HrTag.class.getSuperclass())
+			};
+		} catch(IntrospectionException err) {
+			throw new AssertionError(err);
+		}
+	}
+}
