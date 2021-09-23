25443fe9ee99fab46704684e3782233336f8e51f
AO Industries, Inc <support@aoindustries.com>
2021-09-22 20:34:40 -0500

    Catching SecurityException and logging warning
    Previously, a SecurityException during class initialization, such as when
    trying to access the "user.home" system property, would cause the
    ResourceBundle initialization to fail.  This failure was cached and any
    subsequent attempts to access the bundle, even if it would then succeeed,
    would fail.
    
    With this change, the resource bundle will still function, but without any
    direct access to source files.

--- devel/src/main/java/module-info.java
+++ devel/src/main/java/module-info.java
@@ -24,4 +24,6 @@ module com.aoapps.taglib.devel {
 	exports com.aoapps.taglib.i18n;
 	// Direct
 	requires com.aoapps.hodgepodge; // <groupId>com.aoapps</groupId><artifactId>ao-hodgepodge</artifactId>
+	// Java SE
+	requires java.logging;
 }
