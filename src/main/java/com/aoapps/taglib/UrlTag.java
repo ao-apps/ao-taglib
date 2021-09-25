7cb5275c2e9dfba14e14ff5b9de7d1b68dbf66c7
AO Industries, Inc <support@aoindustries.com>
2021-09-25 16:41:54 -0500

    <ao:url> now supports param.* dynamic attributes and URL manipulations

--- src/main/java/com/aoapps/taglib/UrlTag.java
+++ src/main/java/com/aoapps/taglib/UrlTag.java
@@ -26,22 +26,28 @@ import com.aoapps.encoding.MediaType;
 import com.aoapps.encoding.taglib.EncodingBufferedTag;
 import com.aoapps.io.buffer.BufferResult;
 import com.aoapps.net.MutableURIParameters;
-import com.aoapps.net.URI;
-import com.aoapps.net.URIEncoder;
 import com.aoapps.net.URIParametersMap;
+import com.aoapps.servlet.http.Canonical;
+import com.aoapps.servlet.lastmodified.AddLastModified;
+import com.aoapps.servlet.lastmodified.LastModifiedUtil;
+import com.aoapps.taglib.impl.NoEncodeUrlResponseWrapper;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.DynamicAttributes;
 
 /**
- * TODO: Have absolute option, canonical and lastModified, too?
  * TODO: Replace uses of encoding:text with this as it is now more appropriate for sending dynamic parameters to JavaScript since it calls encodeURL.
  *
  * @author  AO Industries, Inc.
  */
-public class UrlTag extends EncodingBufferedTag implements ParamsAttribute {
+public class UrlTag extends EncodingBufferedTag implements ParamsAttribute, DynamicAttributes {
 
 	public UrlTag() {
 		init();
@@ -68,25 +74,85 @@ public class UrlTag extends EncodingBufferedTag implements ParamsAttribute {
 		params.add(name, value);
 	}
 
+	private boolean absolute;
+	public void setAbsolute(boolean absolute) {
+		this.absolute = absolute;
+	}
+
+	private boolean canonical;
+	public void setCanonical(boolean canonical) {
+		this.canonical = canonical;
+	}
+
+	private AddLastModified addLastModified;
+	public void setAddLastModified(String addLastModified) {
+		this.addLastModified = AddLastModified.valueOfLowerName(addLastModified.trim().toLowerCase(Locale.ROOT));
+	}
+
+/* BodyTag only:
+	private Canonical c;
+/**/
+
+	/**
+	 * @see  ParamUtils#addDynamicAttribute(java.lang.String, java.lang.String, java.lang.Object, java.util.List, com.aoapps.taglib.ParamsAttribute)
+	 */
+	@Override
+	public void setDynamicAttribute(String uri, String localName, Object value) throws JspException {
+		List<String> expectedPatterns = new ArrayList<>();
+		if(!ParamUtils.addDynamicAttribute(uri, localName, value, expectedPatterns, this)) {
+			throw AttributeUtils.newDynamicAttributeFailedException(uri, localName, value, expectedPatterns);
+		}
+	}
+
 	private void init() {
 		params = null;
+		absolute = false;
+		canonical = false;
+		addLastModified = AddLastModified.AUTO;
+/* BodyTag only:
+		if(c != null) {
+			c.close();
+			c = null;
+		}
+/**/
+	}
+
+/* BodyTag only:
+	@Override
+	protected int doStartTag(Writer out) throws JspException, IOException {
+		c = Canonical.set(canonical);
+		return super.doStartTag(out);
 	}
 
 	@Override
-/* BodyTag only:
 	protected int doEndTag(BufferResult capturedBody, Writer out) throws JspException, IOException {
 /**/
 /* SimpleTag only: */
+	@Deprecated
+	@Override
+	public void doTag() throws JspException, IOException {
+		try (Canonical c = Canonical.set(canonical)) {
+			super.doTag();
+		}
+	}
+
+	@Override
 	protected void doTag(BufferResult capturedBody, Writer out) throws JspException, IOException {
 		PageContext pageContext = (PageContext)getJspContext();
 /**/
 		assert capturedBody.trim() == capturedBody : "URLs should have already been trimmed";
-		String url = new URI(capturedBody.toString()).addParameters(params).toString();
-		if(url.startsWith("/")) {
-			HttpServletRequest request = (HttpServletRequest)pageContext.getRequest();
-			URIEncoder.encodeURI(request.getContextPath(), out);
-		}
-		out.write(url);
+		out.write(
+			LastModifiedUtil.buildURL(
+				pageContext.getServletContext(),
+				(HttpServletRequest)pageContext.getRequest(),
+				new NoEncodeUrlResponseWrapper((HttpServletResponse)pageContext.getResponse()),
+				capturedBody.toString(),
+				params,
+				addLastModified,
+				absolute,
+				canonical
+			)
+		);
 /* BodyTag only:
 		return SKIP_PAGE;
 /**/
---
+++ src/main/java/com/aoapps/taglib/impl/NoEncodeUrlResponseWrapper.java
@@ -0,0 +1,55 @@
+/*
+ * ao-taglib - Making JSP be what it should have been all along.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-taglib.
+ *
+ * ao-taglib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-taglib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoapps.taglib.impl;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+/**
+ * Does not encode URL, since that will be done automatically and selectively by tag context.
+ *
+ * @author  AO Industries, Inc.
+ */
+public class NoEncodeUrlResponseWrapper extends HttpServletResponseWrapper {
+
+	public NoEncodeUrlResponseWrapper(HttpServletResponse response) {
+		super(response);
+	}
+
+	/**
+	 * Do not encode URL, since that will be done automatically and selectively by tag context.
+	 */
+	@Override
+	public String encodeURL(String url) {
+		return url;
+	}
+
+	/**
+	 * Do not encode URL, since that will be done automatically and selectively by tag context.
+	 */
+	@Override
+	@Deprecated
+	public String encodeUrl(String url) {
+		return url;
+	}
+}
