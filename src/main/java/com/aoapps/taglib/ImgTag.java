cf4eb3383f0396daad926ef0256c5a3188414ec1
AO Industries, Inc <support@aoindustries.com>
2024-06-13 15:04:34 -0500

    Fixed "alt attribute required"

--- src/main/java/com/aoapps/taglib/ImgTag.java
+++ src/main/java/com/aoapps/taglib/ImgTag.java
@@ -1,6 +1,6 @@
 /*
  * ao-taglib - Making JSP be what it should have been all along.
- * Copyright (C) 2009, 2010, 2011, 2013, 2014, 2015, 2016, 2017, 2019, 2020, 2021, 2022, 2023  AO Industries, Inc.
+ * Copyright (C) 2009, 2010, 2011, 2013, 2014, 2015, 2016, 2017, 2019, 2020, 2021, 2022, 2023, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -40,6 +40,7 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspException;
@@ -133,7 +134,7 @@ public class ImgTag extends ElementBufferedTag
 
   @Override
   public void setAlt(Object alt) {
-    this.alt = Alt.alt.normalize(alt);
+    this.alt = Objects.toString(Alt.alt.normalize(alt), "");
   }
 
   private Object title;
--- src/main/java/com/aoapps/taglib/legacy/ImgTag.java
+++ src/main/java/com/aoapps/taglib/legacy/ImgTag.java
@@ -1,6 +1,6 @@
 /*
  * ao-taglib - Making JSP be what it should have been all along.
- * Copyright (C) 2009, 2010, 2011, 2013, 2014, 2015, 2016, 2017, 2019, 2020, 2021, 2022, 2023  AO Industries, Inc.
+ * Copyright (C) 2009, 2010, 2011, 2013, 2014, 2015, 2016, 2017, 2019, 2020, 2021, 2022, 2023, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -53,6 +53,7 @@ import java.io.ObjectInputStream;
 import java.io.Writer;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspException;
@@ -150,7 +151,7 @@ public class ImgTag extends ElementBufferedBodyTag
 
   @Override
   public void setAlt(Object alt) {
-    this.alt = Alt.alt.normalize(alt);
+    this.alt = Objects.toString(Alt.alt.normalize(alt), "");
   }
 
   private transient Object title;
